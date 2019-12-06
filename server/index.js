cf066b1b20610c3beea22faf55eff4a676d5c446
Adam Weeks <adweeks@cisco.com>
2019-12-06 17:26:34 -0500

    feat(server): add guest endpoint

--- server/index.js
+++ server/index.js
@@ -4,4 +4,21 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+/**
+ * This endpoint does the following things:
+ * Creates a Guest User with the submitted data
+ * Creates a Webex Space
+ * Adds Guest User and "expert" to space
+ * Sends details as a space message
+ */
+app.post('/guest', (req, res) => {
+  // The response should allow the user to open an sdk instance to listen to meetings on the create space.
+  const response = {
+    guestJWT: 'JWT',
+    spaceID: 'space-id',
+  };
+
+  res.json(response);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!!`));

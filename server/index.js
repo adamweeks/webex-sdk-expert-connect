503ea9020542b69bf28c471c61359e73af6090da
Adam Weeks <adweeks@cisco.com>
2020-01-02 14:26:06 -0500

    feat(server): get POST details and send to space

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const app = express();
 const port = 3000;
 const expertEmail = process.env.WEBEX_EXPERT_EMAIL;
 
+app.use(express.json());
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -25,11 +27,15 @@ app.get('/', (req, res) => res.send('Hello World!'));
  */
 app.post('/guest', async (req, res) => {
   // The response should allow the user to open an sdk instance to listen to meetings on the create space.
-
   try {
-    const displayName = 'SDK Workshop Guest';
+    const displayName = req.body.name || 'SDK Workshop Guest';
     const spaceTitle = 'SDK Expert Connect Workshop';
-    const message = 'A user has requested expert support with the following details:';
+    const message = `A user has requested expert support with the following details:
+
+Pet: ${req.body.pet}
+
+Details: ${req.body.details}
+    `;
     const guestJWT = await createUser({displayName});
     const guestUser = await getWebexGuest(guestJWT);
     const space = await prepareSpace({title: spaceTitle, email: expertEmail, guest: guestUser.id, message});
